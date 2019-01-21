db7f70c93f197017dc1d49b97e677ed039b9ae24
leduyphuc2003 <leduyphuc2003@outlook.com>
2019-01-21 17:48:42 +0700

    Bai 5 tao class tuong ung table theo code first

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }  // DateTime? nghĩa là có thể null
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+    }
+}

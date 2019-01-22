5c0321816b3b3aa1e0499e60a5b6b9a6cbb99ea0
leduyphuc2003 <leduyphuc2003@outlook.com>
2019-01-23 01:36:53 +0700

    thêm all class tương ứng với all table trong SQL, het bai 5

--- TeduShop.Model/Models/Post.cs
+++ TeduShop.Model/Models/Post.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Xml.Linq;
 using TeduShop.Model.Abstract;
 
 namespace TeduShop.Model.Models
 {
-    [Table("Products")]
-    public class Product : Auditable
+    [Table("Posts")]
+    public class Post : Auditable
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -18,6 +17,7 @@ namespace TeduShop.Model.Models
 
         [Required]
         [MaxLength(256)]
+        [Column(TypeName = "varchar")]
         public string Alias { set; get; }
 
         [Required]
@@ -26,22 +26,16 @@ namespace TeduShop.Model.Models
         [MaxLength(256)]
         public string Image { set; get; }
 
-        public XElement MoreImage { set; get; }
-        public decimal Price { set; get; }
-
-        public decimal? PromotionPrice { set; get; }// cho phep nullAble
-        public int? Warranty { set; get; }
-
         [MaxLength(500)]
         public string Description { set; get; }
 
-        public string Content { set; get; }
+        public string Content { get; set; }
 
         public bool? HomeFlag { set; get; }
         public bool? HotFlag { set; get; }
         public int? ViewCount { set; get; }
 
         [ForeignKey("CategoryID")]
-        public virtual ProductCategory ProductCategory { set; get; }
+        public virtual PostCategory PostCategory { set; get; }
     }
 }

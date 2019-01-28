2d26b30b9452e0c3b2a9c69464f5e9bad4f046e6
leduyphuc2003 <leduyphuc2003@outlook.com>
2019-01-29 01:44:09 +0700

    bai 7, commit migration and update-database ra database thanh cong trong SQL

--- TeduShop.Model/Models/PostTag.cs
+++ TeduShop.Model/Models/PostTag.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeduShop.Model.Models
 {
     [Table("PostTags")]
-   public  class PostTag
+    public class PostTag
     {
         [Key]
+        [Column(Order = 1)]
         public int PostID { set; get; }
 
         [Key]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar", Order = 2)]
         [MaxLength(50)]
         public string TagID { set; get; }
 
